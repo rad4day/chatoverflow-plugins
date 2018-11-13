1dc1a697482ac9cfce0a3257ff9b1c5a4f694720
sebinside <hi@sebinside.de>
2018-11-13 12:45:57 +0100

    Tested security policies in combination with akka actors. Final note: Just define a framework public ActorSystem and let every input or output define and register their own privileged actor. With this, plugins still have no rights but the context switch inside the input and output API implementation is easy and does not sacrifice type safety.

--- simpletest/src/main/scala/simpletestPlugin.scala
+++ simpletest/src/main/scala/simpletestPlugin.scala
@@ -2,7 +2,7 @@ import org.codeoverflow.chatoverflow.api.plugin.{PluginImpl, PluginManager}
 
 class simpletestPlugin(manager: PluginManager) extends PluginImpl(manager) {
 
-  //private val twitchChatInputReq = require.input.twitchChat("reqTwitch", "A twitch channel", false)
+  private val twitchChatInputReq = require.input.twitchChat("reqTwitch", "A twitch channel", false)
   private val nameToSayHelloToReq = require.parameter.string("reqHello", "Your name", false)
   loopInterval = 1000
 
@@ -11,13 +11,27 @@ class simpletestPlugin(manager: PluginManager) extends PluginImpl(manager) {
     log("Hello!")
     log("Whats up?")
 
-    //twitchChatInputReq.get.registerMessageHandler(msg => println(msg))
+    twitchChatInputReq.get.registerMessageHandler(msg => println(msg))
 
     println(s"Hello ${nameToSayHelloToReq.get}!")
+
+    try {
+      println("\nTest #1: This should fail (no access policy in the plugin)")
+      val output = scala.io.Source.fromURL("https://skate702.de")
+      println(output != null)
+    } catch {
+      case e: Exception => println(s"No rights. Message: ${e.getMessage}")
+    }
+
+    twitchChatInputReq.get.requestURLWithoutRights()
+    twitchChatInputReq.get.requestURLWithoutRightsUsingTheConnector()
+    twitchChatInputReq.get.requestURLUsingStaticActorCode()
+    twitchChatInputReq.get.requestURLUsingDynamicActorExecution()
+    twitchChatInputReq.get.requestURLUsingOwnActor()
   }
 
   override def loop(): Unit = {
-    println("Loop!")
+    //println("Loop!")
   }
 
   override def shutdown(): Unit = {
